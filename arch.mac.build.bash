2c381350c32583a4aceeec9d7f56a47d0982e5e4
Cristian Maglie <c.maglie@bug.st>
2014-06-06 17:07:44 +0200

    Fixed command line parsing for wrapper shell scripts

--- arch.mac.build.bash
+++ arch.mac.build.bash
@@ -11,7 +11,7 @@ then
 	cd objdir/bin
 	mv openocd openocd.bin
 	echo "#!/bin/bash" >> openocd
-	echo "DYLD_LIBRARY_PATH=\"\$(dirname \$0)/../lib\" \$0.bin \$*" >> openocd
+	echo "DYLD_LIBRARY_PATH=\"\$(dirname \$0)/../lib\" \$0.bin \"\$1\" \"\$2\" \"\$3\" \"\$4\" \"\$5\" \"\$6\" \"\$7\" \"\$8\"" >> openocd
 	chmod +x openocd
 	cd -
 fi
