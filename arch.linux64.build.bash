e92aee22573b5f5ff8a53adf868b10cb76e950f5
Cristian Maglie <c.maglie@bug.st>
2014-06-06 14:04:45 +0200

    Added linux64 build script

---
+++ arch.linux64.build.bash
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+CFLAGS="-m64" CXXFLAGS="-m64" HIDAPI_LDFLAGS="-lhidapi-libusb" ./build.all.bash
+
+if [[ -f objdir/bin/openocd ]] ;
+then
+	cd objdir/bin
+	mv openocd openocd.bin
+	echo "#!/bin/bash" >> openocd
+	echo "LD_LIBRARY_PATH=\"\$(dirname \$0)/../lib\" \$0.bin \$*" >> openocd
+	chmod +x openocd
+	cd -
+fi
+
