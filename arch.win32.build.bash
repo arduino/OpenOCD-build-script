a532e7e27cb55cdab6bba0cff6c47bb5179734b7
Cristian Maglie <c.maglie@bug.st>
2014-06-07 14:34:14 +0430

    Bundle cygwin library on win32 build

--- arch.win32.build.bash
+++ arch.win32.build.bash
@@ -2,3 +2,7 @@
 
 CFLAGS="-m32" CXXFLAGS="-m32" HIDAPI_LDFLAGS="-lhidapi" ./build.all.bash
 
+cp /cygdrive/c/cygwin/bin/cygwin1.dll objdir/bin
+cp /cygdrive/c/cygwin/bin/cyggcc_s-1.dll objdir/bin
+
+
