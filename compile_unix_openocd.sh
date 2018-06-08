928cd2d11e2265543d0f7d8794512090933c2605
Martino Facchin <m.facchin@arduino.cc>
2018-06-08 10:14:13 +0200

    Fix unix build

--- compile_unix_openocd.sh
+++ compile_unix_openocd.sh
@@ -91,7 +91,7 @@ else
 export HIDAPI_LIBS="-L$HIDAPI_DIR/mac/.libs/ -L$HIDAPI_DIR/libusb/.libs/ -lhidapi"
 fi
 
-OPENOCD_COMPILE_SWITCHES="--enable-remote-bitbang --enable-stlink --enable-usb-blaster-2 --enable-ti-icdi --enable-jlink --enable-usbprog --enable-cmsis-dap --enable-parport --enable-jtag_vpi --enable-ioutil --enable-bcm2835gpio --enable-remote-bitbang --enable-sysfsgpio --enable-buspirate"
+OPENOCD_COMPILE_SWITCHES="--enable-remote-bitbang --enable-stlink --enable-usb-blaster-2 --enable-ti-icdi --enable-jlink --enable-usbprog --enable-cmsis-dap --enable-jtag_vpi --enable-ioutil --enable-bcm2835gpio --enable-remote-bitbang --enable-sysfsgpio --enable-buspirate"
 
 export CFLAGS="-DHAVE_LIBUSB_ERROR_NAME"
 PKG_CONFIG_PATH=`pwd` ./configure $OPENOCD_COMPILE_SWITCHES --disable-werror --prefix=$PREFIX
