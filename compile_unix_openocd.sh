bb3ebc52caffe3ee12f9ab9a8e3d695181ec4a3f
Martino Facchin <m.facchin@arduino.cc>
2018-06-08 10:47:27 +0200

    Remove sysfs gpio from osx build

--- compile_unix_openocd.sh
+++ compile_unix_openocd.sh
@@ -91,7 +91,11 @@ else
 export HIDAPI_LIBS="-L$HIDAPI_DIR/mac/.libs/ -L$HIDAPI_DIR/libusb/.libs/ -lhidapi"
 fi
 
-OPENOCD_COMPILE_SWITCHES="--enable-remote-bitbang --enable-stlink --enable-usb-blaster-2 --enable-ti-icdi --enable-jlink --enable-usbprog --enable-cmsis-dap --enable-jtag_vpi --enable-ioutil --enable-bcm2835gpio --enable-remote-bitbang --enable-sysfsgpio --enable-buspirate"
+OPENOCD_COMPILE_SWITCHES="--enable-remote-bitbang --enable-stlink --enable-usb-blaster-2 --enable-ti-icdi --enable-jlink --enable-usbprog --enable-cmsis-dap --enable-jtag_vpi --enable-ioutil"
+
+if [[ ${ARCH} != *darwin* ]]; then
+OPENOCD_COMPILE_SWITCHES=$OPENOCD_COMPILE_SWITCHES --enable-sysfsgpio
+fi
 
 export CFLAGS="-DHAVE_LIBUSB_ERROR_NAME"
 PKG_CONFIG_PATH=`pwd` ./configure $OPENOCD_COMPILE_SWITCHES --disable-werror --prefix=$PREFIX
