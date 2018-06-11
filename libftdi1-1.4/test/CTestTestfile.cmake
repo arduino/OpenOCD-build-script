858ac8e7f1e5e796bb3297196a71fd2a46b7133b
Martino Facchin <m.facchin@arduino.cc>
2018-06-11 11:28:09 +0200

    Add libftdi

---
+++ libftdi1-1.4/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /ssd/OpenOCD-build-script/libftdi1-1.4/test
+# Build directory: /ssd/OpenOCD-build-script/libftdi1-1.4/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_libftdi1 "test_libftdi1")
