94f3a82631e4b3e65139942efa1cc30a37f767e4
Martino Facchin <m.facchin@arduino.cc>
2018-06-11 11:28:31 +0200

    Initial cross-compile effort

---
+++ compile_linuxarm_openocd.sh
@@ -0,0 +1,119 @@
+#!/bin/bash -ex
+# Copyright (c) 2016 Arduino LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ARCH=`arm-linux-gnueabihf-gcc -v 2>&1 | awk '/Target/ { print $2 }'`
+
+mkdir -p distrib/$ARCH
+cd  distrib/$ARCH
+PREFIX=`pwd`
+cd -
+
+#disable pkg-config
+export PKG_CONFIG_PATH=`pwd`
+
+if [[ ${ARCH} != *darwin* ]]; then
+
+cd eudev-3.1.5
+export UDEV_DIR=`pwd`
+./autogen.sh
+./configure --enable-static --disable-shared --disable-blkid --disable-kmod  --disable-manpages --host=arm-linux-gnueabihf
+make clean
+make -j4
+cd ..
+
+export CFLAGS="-I$UDEV_DIR/src/libudev/"
+export LDFLAGS="-L$UDEV_DIR/src/libudev/.libs/"
+export LIBS="-ludev"
+
+fi
+
+cd libusb-1.0.20
+export LIBUSB_DIR=`pwd`
+./configure --enable-static --disable-shared --host=arm-linux-gnueabihf
+make clean
+make
+cd ..
+
+export LIBUSB1_CFLAGS="-I$LIBUSB_DIR/libusb/"
+export LIBUSB1_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread"
+
+export LIBUSB_1_0_CFLAGS="-I$LIBUSB_DIR/libusb/"
+export LIBUSB_1_0_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread"
+
+cd libusb-compat-0.1.5
+export LIBUSB0_DIR=`pwd`
+automake --add-missing
+autoreconf
+./configure --enable-static --disable-shared --host=arm-linux-gnueabihf
+make clean
+make
+cd ..
+
+export libusb_CFLAGS="-I$LIBUSB_DIR/libusb/"
+export libusb_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread"
+export libudev_CFLAGS="-I$UDEV_DIR/src/libudev/"
+export libudev_LIBS="-L$UDEV_DIR/src/libudev/.libs/ -ludev"
+
+cd hidapi
+./bootstrap
+export HIDAPI_DIR=`pwd`
+./configure --enable-static --disable-shared --host=arm-linux-gnueabihf
+make clean
+make -j4
+cd ..
+
+cd libftdi1-1.4
+export LIBFTDI1_DIR=`pwd`
+if [ -d build ]; then
+rm -rf build
+fi
+mkdir build && cd build
+cmake -DCMAKE_C_COMPILER=arm-linux-gnueabihf-gcc -DBUILD_TESTS=no -DDOCUMENTATION=no -DEXAMPLES=no -DFTDIPP=no -DFTDI_EEPROM=no -DLINK_PYTHON_LIBRARY=no -DPYTHON_BINDINGS=no -DCMAKE_INSTALL_PREFIX=$LIBFTDI1_DIR ..
+cd ..
+make clean
+make ftdi1-static
+cd ..
+
+cd OpenOCD
+./bootstrap
+export LIBUSB0_CFLAGS="-I$LIBUSB0_DIR/libusb/" 
+export LIBUSB0_LIBS="-L$LIBUSB0_DIR/libusb/.libs/ -lusb -lpthread" 
+export LIBUSB1_CFLAGS="-I$LIBUSB_DIR/libusb/" 
+export LIBUSB1_LIBS="-L$LIBUSB_DIR/libusb/.libs/ -lusb-1.0 -lpthread" 
+export LIBFTDI_CFLAGS="-I$LIBFTDI1_DIR/src/"
+export LIBFTDI_LIBS="-L$LIBFTDI1_DIR/src/ -lftdi1 -lpthread"
+export HIDAPI_CFLAGS="-I$HIDAPI_DIR/hidapi/"
+
+if [[ ${ARCH} != *darwin* ]]; then
+
+export HIDAPI_LIBS="-L$HIDAPI_DIR/linux/.libs/ -L$HIDAPI_DIR/libusb/.libs/ -lhidapi-hidraw -lhidapi-libusb" 
+
+else
+
+export HIDAPI_LIBS="-L$HIDAPI_DIR/mac/.libs/ -L$HIDAPI_DIR/libusb/.libs/ -lhidapi"
+fi
+
+OPENOCD_COMPILE_SWITCHES="--enable-remote-bitbang --enable-stlink --enable-usb-blaster-2 --enable-ti-icdi --enable-jlink --enable-usbprog --enable-cmsis-dap --enable-jtag_vpi --enable-ioutil"
+
+if [[ ${ARCH} != *darwin* ]]; then
+OPENOCD_COMPILE_SWITCHES="$OPENOCD_COMPILE_SWITCHES --enable-sysfsgpio"
+fi
+
+export CFLAGS="-DHAVE_LIBUSB_ERROR_NAME"
+PKG_CONFIG_PATH=`pwd` ./configure $OPENOCD_COMPILE_SWITCHES --disable-werror --prefix=$PREFIX --host=arm-linux-gnueabihf
+make clean
+CFLAGS=-static make
+make install
